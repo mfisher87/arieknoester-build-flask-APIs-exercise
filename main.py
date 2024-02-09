350ea7194049b12333118e5f786677cb5319519f
Arie Knoester <arikstr@gmail.com>
2024-02-09 15:56:10 -0600

    Removed unused imports.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template, request, redirect, url_for
+from flask import Flask, jsonify, render_template, request
 from models.cafe import Cafe, db
 import random
 
