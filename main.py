47e68e24c777775bf2f13a7201df8a495eee806a
Arie Knoester <arikstr@gmail.com>
2024-02-09 11:02:48 -0600

    Moved db related code to ./models

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from flask import Flask, jsonify, render_template, request
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import Integer, String, Boolean
+from models.cafe import Cafe, db
 
 '''
 Install the required packages first: 
@@ -18,31 +16,9 @@ This will install the packages from requirements.txt for this project.
 
 app = Flask(__name__)
 
-
-# CREATE DB
-class Base(DeclarativeBase):
-    pass
-
-
 # Connect to Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
-db = SQLAlchemy(model_class=Base)
 db.init_app(app)
-
-
-# Cafe TABLE Configuration
-class Cafe(db.Model):
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    name: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
-    map_url: Mapped[str] = mapped_column(String(500), nullable=False)
-    img_url: Mapped[str] = mapped_column(String(500), nullable=False)
-    location: Mapped[str] = mapped_column(String(250), nullable=False)
-    seats: Mapped[str] = mapped_column(String(250), nullable=False)
-    has_toilet: Mapped[bool] = mapped_column(Boolean, nullable=False)
-    has_wifi: Mapped[bool] = mapped_column(Boolean, nullable=False)
-    has_sockets: Mapped[bool] = mapped_column(Boolean, nullable=False)
-    can_take_calls: Mapped[bool] = mapped_column(Boolean, nullable=False)
-    coffee_price: Mapped[str] = mapped_column(String(250), nullable=True)
 
 
 with app.app_context():
